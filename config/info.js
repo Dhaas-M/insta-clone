333dd78f4ee8bc4d9fd40dd2ae042944a18b7645
Dhaas-M <94614733+Dhaas-M@users.noreply.github.com>
2025-09-21 11:25:21 +0530

    Update info.js

--- config/info.js
+++ config/info.js
@@ -1,7 +1,4 @@
-// module.exports={
-//     MONGOURI:'mongodb+srv://Dhaas:Dha%40s123@cluster0.iqrrzh1.mongodb.net/?retryWrites=true&w=majority',
-//     JWT_SECRET:'qwertyuioplkjhgfdsazxcvbnm'
-// }
+
 
 if(process.env.NODE_ENV === 'production') {
     module.exports = require('./prod')
